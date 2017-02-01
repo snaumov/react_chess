d469c68c290e1fa1d5796afa2267c2b416703f63
snaumov <s.s.naumov@gmail.com>
2017-02-01 11:56:34 +0300

    added header

--- src/UI/components/header.js
+++ src/UI/components/header.js
@@ -6,7 +6,7 @@ class Header extends React.Component {
         return(
             <div className="header">
                 <div className="logo">
-                    <img src={require("./media/site_logo3.png")} alt="ReactLogo"/>
+                    <img src={require("./media/site_logo.png")} alt="ReactLogo"/>
                     <p className="siteHeader">ReactChess</p>
                 </div>
                 <div className="headerControlElements">

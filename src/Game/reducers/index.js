a5cc0b3a10edbb243c02762c2e981a60ac7755cb
snaumov <s.s.naumov@gmail.com>
2017-02-06 12:00:44 +0300

    styling for movesList

--- src/Game/reducers/index.js
+++ src/Game/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux'
 import { MAKE_MOVE, UPDATE_START_SQUARE, JUMP_TO, START_NEW_GAME, RESIGN } from '../actions'
 import Chess from 'chess.js'
 
+
 const initialState = {
     chess: new Chess(),
     position: {
@@ -41,6 +42,7 @@ const initialState = {
     },
     whiteIsNext: true,
     startSquare: '',
+    pieceToMoveLetter: '',
     history: [],
     availableMoves: [],
     fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
@@ -49,6 +51,17 @@ const initialState = {
     checkMate: false,
 }
 
+function pieceToMoveLetterHelper(pieceName) {
+    const unicodePictureCodes = {
+        'K': '\u2654',
+        'Q': '\u2655',
+        'R': '\u2656',
+        'B': '\u2657',
+        'N': '\u2658'
+    }
+
+    return pieceName[1] === 'P' ? '' : pieceName[2] === 'n' ? unicodePictureCodes['N'] : unicodePictureCodes[pieceName[1]]
+}
 
 function position(state=initialState, action) {
     switch(action.type) {
@@ -61,6 +74,7 @@ function position(state=initialState, action) {
             console.log(state);
             return Object.assign({}, state, {
                 startSquare: action.startSquare,
+                pieceToMoveLetter: pieceToMoveLetterHelper(state.position[action.startSquare]),
                 availableMoves: [].concat(state.chess.moves({square: action.startSquare}))
             })
         case MAKE_MOVE:
@@ -70,9 +84,22 @@ function position(state=initialState, action) {
                     resigned: true,
                 })
             }
+            
+            // create a variable for storing target square to show in history
+            var endSquareForHistory = action.targetSquare;
 
+            //create a copy of current position and update it
             var newPosition = Object.assign({}, state.position);
             var pieceToMove = newPosition[state.startSquare];
+
+            if (newPosition[action.targetSquare] !== undefined){
+                if(state.pieceToMoveLetter === ''){
+                    endSquareForHistory = state.startSquare[0] + 'x' + endSquareForHistory;
+                } else {
+                    endSquareForHistory = 'x' + endSquareForHistory;
+                }              
+            }
+
             newPosition[state.startSquare] = undefined;
             newPosition[action.targetSquare] = pieceToMove;
 
@@ -80,18 +107,22 @@ function position(state=initialState, action) {
             if (pieceToMove === 'wKing' && state.startSquare === 'e1'){
                 if(action.targetSquare === 'g1') {
                     newPosition['h1'] = undefined;
-                    newPosition['f1'] = 'wRook';                  
+                    newPosition['f1'] = 'wRook';
+                    endSquareForHistory = 'O-O';                  
                 } else if (action.targetSquare === 'c1') {
                     newPosition['a1'] = undefined;
                     newPosition['d1'] = 'wRook';
+                    endSquareForHistory = 'O-O-O';
                 }
             } else if (pieceToMove === 'bKing' && state.startSquare === 'e8') {
                 if(action.targetSquare === 'g8') {
                     newPosition['h8'] = undefined;
-                    newPosition['f8'] = 'bRook';                  
+                    newPosition['f8'] = 'bRook';
+                    endSquareForHistory = 'O-O';                  
                 } else if (action.targetSquare === 'c8') {
                     newPosition['a8'] = undefined;
                     newPosition['d8'] = 'bRook';
+                    endSquareForHistory = 'O-O-O';
                 }            
             }
             
@@ -102,7 +133,7 @@ function position(state=initialState, action) {
             return Object.assign({}, state, {
                     position: newPosition,    
                     whiteIsNext: !state.whiteIsNext,
-                    history: [...state.history, {endSquare: action.targetSquare, position: newPosition}],
+                    history: [...state.history, {endSquare: endSquareForHistory[0] !== 'O' ? state.pieceToMoveLetter + endSquareForHistory : endSquareForHistory, position: newPosition}],
                     chess: newChess,
                     fen: newFen,
                     checkMate: newChess.in_checkmate(),

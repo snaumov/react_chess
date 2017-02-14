9749ba82c1780ab907bfb6c5a1e7d39545a19914
snaumov <s.s.naumov@gmail.com>
2017-02-14 10:04:28 +0300

    in continue to prev commit

--- src/Game/containers/GameWithEngine.js
+++ src/Game/containers/GameWithEngine.js
@@ -35,11 +35,11 @@ class GameWithEngineComponent extends GameComponent {
   }
 
   render() {
-    const { position, whiteIsNext, availableMoves, history, whiteAtBottom, resigned } = this.props.position
+    const { positionToShow, whiteIsNext, availableMoves, history, whiteAtBottom, resigned } = this.props.position
     return (
       <div className="gameContainer">
         <div className="chessBoard">
-          <Board position={position} whiteIsNext={whiteIsNext} onClick={this.onMouseDown} availableMoves={availableMoves} onMouseUp={this.onMouseUp} whiteAtBottom={whiteAtBottom} resigned={resigned}/>
+          <Board position={positionToShow} whiteIsNext={whiteIsNext} onClick={this.onMouseDown} availableMoves={availableMoves} onMouseUp={this.onMouseUp} whiteAtBottom={whiteAtBottom} resigned={resigned}/>
         </div>
         {this.props.ui.showNewGamePopup ? <NewGamePopup onClick={this.props.ui.newGamePopupLinksTo === '/' ? this.onClickNewNetworkGame : this.onClickNewGame} onChange={this.handlePopupChange} onCloseClick={() => this.props.dispatch(hideNewGamePopup())} newGamePopupLinksTo={this.props.ui.newGamePopupLinksTo} /> : undefined}
         <MovesList history={history} onClick={this.jumpTo} currentMoveNumber={this.props.position.moveNumber} whiteSideUsername={whiteAtBottom ? this.props.ui.username : 'Engine'} blackSideUsername={whiteAtBottom ? 'Engine' : this.props.ui.username}/>
